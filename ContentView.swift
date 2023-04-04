e23591b1a20e38d88b5bdf064aa081b8aa2ac4c3
Ali Ibrahim <alialchi07@gmail.com>
2023-04-04 14:39:22 -0500

    Commit 3 | Person 1

--- ContentView.swift
+++ ContentView.swift
@@ -5,21 +5,48 @@ struct ContentView: View {
     @State var integer2: Int = 0
     @State var answer: Int = 0
     var body: some View {
+        Text("Calculator")
+            .font(.system(size: 30))
         TextField("Number 1", value: $integer1, format: .number)
+            .textFieldStyle(.roundedBorder)
+            .padding()
         TextField("Number 2", value: $integer2, format: .number)
-        Button("Multiply") {
-            answer = integer1 *  integer2
+            .textFieldStyle(.roundedBorder)
+            .padding()
+        HStack {
+            Button("Multiply") {
+                answer = integer1 *  integer2
+            }
+            .frame(width: 50, height: 50)
+            .background(.red)
+            .padding()
+            .foregroundColor(.black)
+            Button("+") {
+                
+            }
+            .frame(width: 50, height: 50)
+            .background(.blue)
+            .padding()
+            .foregroundColor(.black)
+            
+            Button("-") {
+                
+            }
+            .frame(width: 50, height: 50)
+            .background(.yellow)
+            .padding()
+            .foregroundColor(.black)
+            
+            Button("/") {
+                
+            }
+            .frame(width: 50, height: 50)
+            .background(.green)
+            .padding()
+            .foregroundColor(.black)
+            
         }
         Text("\(answer)")
-        Button("+") {
-            
-        }
-        Button("-") {
-            
-        }
-        Button("/") {
-            
-        }
         
     }
 }
